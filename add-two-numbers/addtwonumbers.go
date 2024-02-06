6235d5090b66d51a3b5a675bff69b507c3267b01
vyacheslav <vyacheslav.soynov@gmail.com>
2024-02-06 20:45:35 +0700

    feat: outputs nil for nil list

--- add-two-numbers/addtwonumbers.go
+++ add-two-numbers/addtwonumbers.go
@@ -1,1 +1,17 @@
 package addtwonumbers
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	return nil
+}
--- add-two-numbers/addtwonumbers_test.go
+++ add-two-numbers/addtwonumbers_test.go
@@ -1,3 +1,10 @@
 package addtwonumbers
 
 import "testing"
+
+func TestOutputsNilForNilLists(t *testing.T) {
+	result := addTwoNumbers(nil, nil)
+	if result != nil {
+		t.Errorf("Result must be nil when list is nil, result: %v", result)
+	}
+}
