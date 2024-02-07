3c9edc78702a408f57193f58a7d8bc77b95022f9
vyacheslav <vyacheslav.soynov@gmail.com>
2024-02-07 09:03:56 +0700

    feat: complete add-two-numbers problem

--- add-two-numbers/addtwonumbers.go
+++ add-two-numbers/addtwonumbers.go
@@ -1,7 +1,7 @@
 package addtwonumbers
 
 type ListNode struct {
-	Val int
+	Val  int
 	Next *ListNode
 }
 
@@ -13,8 +13,51 @@ type ListNode struct {
  * }
  */
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	if l1 == nil || l2 == nil {
+	if l1 == nil && l2 == nil {
 		return nil
+	} else if l1 == nil {
+		return addTwoNumbers(&ListNode{Val: 0}, l2)
+	} else if l2 == nil {
+		return addTwoNumbers(l1, &ListNode{Val: 0})
 	}
-	return &ListNode{}
+	sum := l1.Val + l2.Val
+	if sum > 9 {
+		tens := sum / 10
+		units := sum % 10
+		if l1.Next != nil {
+			return &ListNode{
+				Val: units,
+				Next: addTwoNumbers(
+					&ListNode{
+						Val:  l1.Next.Val + tens,
+						Next: l1.Next.Next,
+					},
+					l2.Next,
+				),
+			}
+		}
+		if l2.Next != nil {
+			return &ListNode{
+				Val: units,
+				Next: addTwoNumbers(
+					l1.Next,
+					&ListNode{
+						Val:  l2.Next.Val + tens,
+						Next: l2.Next.Next,
+					},
+				),
+			}
+		}
+		return &ListNode{
+			Val:  units,
+			Next: &ListNode{Val: tens},
+		}
+	}
+	if l1.Next != nil || l2.Next != nil {
+		return &ListNode{
+			Val:  sum,
+			Next: addTwoNumbers(l1.Next, l2.Next),
+		}
+	}
+	return &ListNode{Val: sum}
 }
--- add-two-numbers/addtwonumbers_test.go
+++ add-two-numbers/addtwonumbers_test.go
@@ -14,6 +14,75 @@ func TestSumsEmptyListsToEmptyList(t *testing.T) {
 	l2 := &ListNode{}
 	result := addTwoNumbers(l1, l2)
 	if result.Val != 0 || result.Next != nil {
-		t.Errorf("Results must be zero when lists is empty, result %v", result)
+		t.Errorf("Result must be zero when lists is empty, result: %v", result)
+	}
+}
+
+func TestSumsSingletonLists(t *testing.T) {
+	l1 := &ListNode{Val: 1}
+	l2 := &ListNode{Val: 1}
+	result := addTwoNumbers(l1, l2)
+	if result.Val != 2 || result.Next != nil {
+		t.Errorf("Result must be a sum two nums without added nums, result: %v", result)
+	}
+}
+
+func TestSumsSingletonListsWithOverflow(t *testing.T) {
+	l1 := &ListNode{Val: 5}
+	l2 := &ListNode{Val: 5}
+	result := addTwoNumbers(l1, l2)
+	if result.Val != 0 || result.Next.Val != 1 {
+		t.Errorf("Result must be overflowed from first value, result: %v", result)
+	}
+}
+
+func TestSumsSymmetricMultipleLists(t *testing.T) {
+	l1 := &ListNode{Val: 1, Next: &ListNode{Val: 1}}
+	l2 := &ListNode{Val: 1, Next: &ListNode{Val: 1}}
+	result := addTwoNumbers(l1, l2)
+	if result.Val != 2 || result.Next.Val != 2 {
+		t.Errorf("Must sum the symmetric multiple lists without overflow, result: %v", result)
+	}
+}
+
+func TestSumsSymmetricMultipleListsWithOverflow(t *testing.T) {
+	l1 := &ListNode{Val: 5, Next: &ListNode{Val: 1}}
+	l2 := &ListNode{Val: 5, Next: &ListNode{Val: 1}}
+	result := addTwoNumbers(l1, l2)
+	if result.Val != 0 || result.Next.Val != 3 {
+		t.Errorf("Must sum the symmetric lists with overflow, first: %v, second: %v", result.Val, result.Next.Val)
+	}
+}
+
+func TestSumsUnsymmetricLists(t *testing.T) {
+	l1 := &ListNode{Val: 1}
+	l2 := &ListNode{Val: 0, Next: &ListNode{Val: 1}}
+	result := addTwoNumbers(l1, l2)
+	if result.Val != 1 || result.Next.Val != 1 {
+		t.Errorf("Must sum the unsymmetric lists without overflow, first: %v, second: %v", result.Val, result.Next.Val)
+	}
+}
+
+func TestSumsUnsymmetricListsWithOverflow(t *testing.T) {
+	l1 := &ListNode{Val: 5}
+	l2 := &ListNode{Val: 5, Next: &ListNode{Val: 1}}
+	result := addTwoNumbers(l1, l2)
+	if result.Val != 0 || result.Next.Val != 2 {
+		t.Errorf("Must sum the unsymmetric lists with overflow, first: %v, second: %v", result.Val, result.Next.Val)
+	}
+}
+
+func TestSumsUnsymmetricListsWithManyOverflow(t *testing.T) {
+	l1 := &ListNode{Val: 9, Next: &ListNode{Val: 9}}
+	l2 := &ListNode{Val: 9, Next: &ListNode{Val: 9, Next: &ListNode{Val: 9}}}
+	result := addTwoNumbers(l1, l2)
+	if result.Val != 8 || result.Next.Val != 9 || result.Next.Next.Val != 0 || result.Next.Next.Next.Val != 1 {
+		t.Errorf(
+			"Must sum the unsymmetric lists with many overflow, nums: %v %v %v %v",
+			result.Val,
+			result.Next.Val,
+			result.Next.Next.Val,
+			result.Next.Next.Next.Val,
+		)
 	}
 }
