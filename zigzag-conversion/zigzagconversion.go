296b6313d88597ba2a8dc4a165bb4e85a003c7fc
vyacheslav <vyacheslav.soynov@gmail.com>
2024-03-06 07:56:46 +0700

    feat: complete zigzag-conversion problem

--- zigzag-conversion/zigzagconversion.go
+++ zigzag-conversion/zigzagconversion.go
@@ -2,18 +2,22 @@ package zigzagconversion
 
 func convert(s string, numRows int) string {
 	var result string
-	var offset, startsRow int
+	var offset, currentRow int
 	periodicOffset := numRows + max(numRows-2, 0)
+	hasDiagonalChars := periodicOffset != numRows
 	for len(result) != len(s) {
 		result += string(s[offset])
-		if periodicOffset != numRows && startsRow != (numRows-1) {
-			offset += periodicOffset - startsRow - startsRow
-		} else {
-			offset += periodicOffset
+		if hasDiagonalChars {
+			isNotFirstOrLastRow := currentRow != (numRows-1) && currentRow != 0
+			diagonalOffset := offset + periodicOffset - (currentRow * 2)
+			if isNotFirstOrLastRow && diagonalOffset < len(s) {
+				result += string(s[diagonalOffset])
+			}
 		}
+		offset += periodicOffset
 		if len(s) <= offset {
-			startsRow += 1
-			offset = startsRow
+			currentRow += 1
+			offset = currentRow
 		}
 	}
 	return result
