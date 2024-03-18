592a83144e97a907e19cc7ce9f7ca2a9eca65df7
vyacheslav <vyacheslav.soynov@gmail.com>
2024-03-18 08:30:00 +0700

    feat: convert string to number without clamping for overflow

--- string-to-integer-atoi/stringtointegeratoi.go
+++ string-to-integer-atoi/stringtointegeratoi.go
@@ -1,5 +1,25 @@
 package stringtointegeratoi
 
+var charToDigitMap = map[byte]int{'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+
 func myAtoi(s string) int {
-	return 0
+	if len(s) == 0 {
+		return 0
+	}
+	if s[0] == ' ' {
+		return myAtoi(s[1:])
+	}
+	if s[0] == '-' {
+		return myAtoi(s[1:]) * -1
+	}
+	var result int
+	for position, awaitDigit := 0, true; awaitDigit && position != len(s); position += 1 {
+		value, isDigit := charToDigitMap[s[position]]
+		if isDigit {
+			result *= 10
+			result += value
+		}
+		awaitDigit = isDigit
+	}
+	return result
 }
--- string-to-integer-atoi/stringtointegeratoi_test.go
+++ string-to-integer-atoi/stringtointegeratoi_test.go
@@ -8,3 +8,43 @@ func TestConvertsEmptyStringToZero(t *testing.T) {
 		t.Errorf("Empty string must be converted to zero, actual [%v]", result)
 	}
 }
+
+func TestConvertsOneDigitString(t *testing.T) {
+	s := "1"
+	result := myAtoi(s)
+	if result != 1 {
+		t.Errorf("String with one digit [%v] must returns [1], actual [%v]", s, result)
+	}
+}
+
+func TestConvertsOneDigitWithWhitespace(t *testing.T) {
+	s := " 2"
+	result := myAtoi(s)
+	if result != 2 {
+		t.Errorf("One digit string with leading whitespace [%v] must converting to [2], actual [%v]", s, result)
+	}
+}
+
+func TestConvertsNegativeOneDigitString(t *testing.T) {
+	s := "-1"
+	result := myAtoi(s)
+	if result != -1 {
+		t.Errorf("String with leading minus [%v] must converting to [-1], actual [%v]", s, result)
+	}
+}
+
+func TestConvertsTwoDigitsString(t *testing.T) {
+	s := "12"
+	result := myAtoi(s)
+	if result != 12 {
+		t.Errorf("String with two digits [%v] must converting to [12], actual [%v]", s, result)
+	}
+}
+
+func TestConvertsWithClampToMaxNumber(t *testing.T) {
+	s := "2147483648"
+	result := myAtoi(s)
+	if result != 2147483647 {
+		t.Errorf("String with overflow [%v] must be clamped to [2147483647], actual [%v]", s, result)
+	}
+}
