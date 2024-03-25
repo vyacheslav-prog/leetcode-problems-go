c530c80ba00f3cdfab15d256e89e2b0bca3729b9
vyacheslav <vyacheslav.soynov@gmail.com>
2024-03-25 08:18:42 +0700

    feat: detect pattern for single occurrence him

--- regular-expression-matching/regularexpressionmatching.go
+++ regular-expression-matching/regularexpressionmatching.go
@@ -1,5 +1,21 @@
 package regularexpressionmatching
 
+const (
+	anyCharPattern = "anychar"
+	charPattern = "char"
+	zeroOrMoreCharPattern = "zeroormorechar"
+)
+
+func detectNextPattern(p string) string {
+	if len(p) > 1 {
+		return zeroOrMoreCharPattern
+	}
+	if p == "." {
+		return anyCharPattern
+	}
+	return "char"
+}
+
 func isMatch(s string, p string) bool {
-	return p == ""
+	return p == s
 }
--- regular-expression-matching/regularexpressionmatching_test.go
+++ regular-expression-matching/regularexpressionmatching_test.go
@@ -26,3 +26,35 @@ func TestPassesForEqualsCharPatternAndString(t *testing.T) {
 		t.Errorf("Result must be [true] for equal char pattern [%v] and string [%v], actual is [%v]", p, s, result)
 	}
 }
+
+func TestDetectsCharPattern(t *testing.T) {
+	p := "a"
+	result := detectNextPattern(p)
+	if result != charPattern {
+		t.Errorf("Result must be [char] for detected pattern [%v], actual is [%v]", p, result)
+	}
+}
+
+func TestDetectsAnyCharPattern(t *testing.T) {
+	p := "."
+	result := detectNextPattern(p)
+	if result != anyCharPattern {
+		t.Errorf("Result must be [anychar] for detected pattern [%v], actual is [%v]", p, result)
+	}
+}
+
+func TestDetectsZeroOrMoreCharPattern(t *testing.T) {
+	p := "a*"
+	result := detectNextPattern(p)
+	if result != zeroOrMoreCharPattern {
+		t.Errorf("Result must be [zeroormorechar] for detected pattern [%v], actual is [%v]", p, result)
+	}
+}
+
+func TestDetectsCharPatternForTwoCharString(t *testing.T) {
+	p := "ab"
+	result := detectNextPattern(p)
+	if result != charPattern {
+		t.Errorf("Result must be [char] for detected pattern [%v] with two char, actual is [%v]", p, result)
+	}
+}
