05ab1755dd9c6ed2fb8cdfa3ae44c4d58b00fbbd
vyacheslav <vyacheslav.soynov@gmail.com>
2024-04-24 08:00:19 +0700

    feat: calc a water container for two lines

--- container-with-most-water/containerwithmostwater.go
+++ container-with-most-water/containerwithmostwater.go
@@ -1,5 +1,11 @@
 package containerwithmostwater
 
 func maxArea(height []int) int {
-	return 1
+	if len(height) < 2 {
+		return 0
+	}
+	if height[1] < height[0] {
+		return height[1]
+	}
+	return height[0]
 }
--- container-with-most-water/containerwithmostwater_test.go
+++ container-with-most-water/containerwithmostwater_test.go
@@ -8,3 +8,43 @@ func TestEmptyWaterForEmptyContainer(t *testing.T) {
 		t.Errorf("Result must be zero for empty container, actual is [%v]", result)
 	}
 }
+
+func TestStoresOneAmountForTwoSingleHeightLines(t *testing.T) {
+	height := []int{1, 1}
+	result := maxArea(height)
+	if expected := 1; result != expected {
+		t.Errorf("Result must be [%v] for two single-height lines [%v], actual is [%v]", expected, height, result)
+	}
+}
+
+func TestStoresZeroAmountForOneLine(t *testing.T) {
+	height := []int{2}
+	result := maxArea(height)
+	if result != 0 {
+		t.Errorf("Result must be zero for one line [%v], actual is [%v]", height, result)
+	}
+}
+
+func TestStoresOneAmountForTwoDescendingLinesWithMinimumOfOne(t *testing.T) {
+	height := []int{2, 1}
+	result := maxArea(height)
+	if expected := 1; result != expected {
+		t.Errorf("Result must be [%v] for a descending order lines [%v], actual is [%v]", expected, height, result)
+	}
+}
+
+func TestStoresOneAmountForTwoAscendingLinesWithMinimumOfOne(t *testing.T) {
+	height := []int{1, 2}
+	result := maxArea(height)
+	if expected := 1; result != expected {
+		t.Errorf("Result must be [%v] for a ascending order lines [%v], actual is [%v]", expected, height, result)
+	}
+}
+
+func TestStoresTwoAmountForThreeSingleHeightLines(t *testing.T) {
+	height := []int{1, 1, 1}
+	result := maxArea(height)
+	if expected := 2; result != expected {
+		t.Errorf("Result must be [%v] for three single-height lines [%v], actual is [%v]", expected, height, result)
+	}
+}
