f47d0f3051848725b4a47efdca22ca50cbbd4edd
vyacheslav <vyacheslav.soynov@gmail.com>
2024-05-23 07:44:37 +0700

    feat: convert single units and fives for number

--- integer-to-roman/integertoroman.go
+++ integer-to-roman/integertoroman.go
@@ -1,5 +1,11 @@
 package integertoroman
 
+var numberToSymbolMap = map[int]string{
+	0: "",
+	1: "I",
+	5: "V",
+}
+
 func intToRoman(num int) string {
-	return ""
+	return numberToSymbolMap[num]
 }
--- integer-to-roman/integertoroman_test.go
+++ integer-to-roman/integertoroman_test.go
@@ -8,3 +8,27 @@ func TestRepresentsEmptyStringForZero(t *testing.T) {
 		t.Errorf("Result must be empty string for number [%v], actual is [%v]", 0, result)
 	}
 }
+
+func TestConvertsUnitAsI(t *testing.T) {
+	num := 1
+	result := intToRoman(num)
+	if expected := "I"; expected != result {
+		t.Errorf("Result must be [%v] for number [%v], actual is [%v]", expected, num, result)
+	}
+}
+
+func TestConvertsFiveAsV(t *testing.T) {
+	num := 5
+	result := intToRoman(num)
+	if expected := "V"; expected != result {
+		t.Errorf("Result must be [%v] for number [%v], actual is [%v]", expected, num, result)
+	}
+}
+
+func TestConvertsTwoAsRepeatedUnitSymbol(t *testing.T) {
+	num := 2
+	result := intToRoman(num)
+	if expected := "II"; expected != result {
+		t.Errorf("Result must be [%v] for number [%v], actual is [%v]", expected, num, result)
+	}
+}
