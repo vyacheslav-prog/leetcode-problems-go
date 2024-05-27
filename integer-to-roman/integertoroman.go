0364822cd3d6b3f8f4cca9f07fd4f6ea5f5967b9
vyacheslav <vyacheslav.soynov@gmail.com>
2024-05-27 08:05:39 +0700

    feat: complete integer-to-roman problem

--- integer-to-roman/integertoroman.go
+++ integer-to-roman/integertoroman.go
@@ -1,11 +1,16 @@
 package integertoroman
 
-var numberToSymbolKeys = []int{1000, 900, 100, 10, 9, 5, 4, 1, 0}
+var numberToSymbolKeys = []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1, 0}
 
 var numberToSymbolMap = map[int]string{
 	1000: "M",
 	900:  "CM",
+	500:  "D",
+	400:  "CD",
 	100:  "C",
+	90:   "XC",
+	50:   "L",
+	40:   "XL",
 	10:   "X",
 	9:    "IX",
 	5:    "V",
