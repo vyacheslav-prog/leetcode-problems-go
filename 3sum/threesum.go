2e434f03da516199f00205cc63415674d8449309
vyacheslav <vyacheslav.soynov@gmail.com>
2024-06-04 07:38:30 +0700

    feat: find a first triplet when total sum is not zero

--- 3sum/threesum.go
+++ 3sum/threesum.go
@@ -1,8 +1,15 @@
 package threesum
 
 func threeSum(nums []int) [][]int {
-	if 0 == len(nums) {
-		return [][]int{}
+	var result [][]int
+	if 2 < len(nums) {
+		var sum int
+		for _, number := range nums[:3] {
+			sum += number
+		}
+		if 0 == sum {
+			result = append(result, nums)
+		}
 	}
-	return [][]int{nums}
+	return result
 }
--- 3sum/threesum_test.go
+++ 3sum/threesum_test.go
@@ -24,3 +24,19 @@ func TestFindsZeroTripletsForNumsWithoutZeroSum(t *testing.T) {
 		t.Errorf("Result must be empty for nums without a zero sum [%v], actual is [%v]", nums, result)
 	}
 }
+
+func TestFindsFirstTripletForFourNumsWhenTotalSumIsNotZero(t *testing.T) {
+	nums := []int{0, 0, 0, 1}
+	result := threeSum(nums)
+	if expected := [3]int{0, 0, 0}; 1 != len(result) || expected != [3]int(result[0]) {
+		t.Errorf("Result must have [%v] for first triplet for nums [%v], actual is [%v]", expected, nums, result)
+	}
+}
+
+func TestFindsLastTripletForFourNumsWhenTotalSumIsNotZero(t *testing.T) {
+	nums := []int{1, 0, 0, 0}
+	result := threeSum(nums)
+	if expected := [3]int{0, 0, 0}; 1 != len(result) || expected != [3]int(result[0]) {
+		t.Errorf("Result must have [%v] for last triplet for nums [%v], actual is [%v]", expected, nums, result)
+	}
+}
