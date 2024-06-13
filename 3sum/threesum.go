fead010a94597bd100585fe55bcd9d62dcc147ff
vyacheslav <vyacheslav.soynov@gmail.com>
2024-06-13 08:28:18 +0700

    feat: find a unique triplets by same balance

--- 3sum/threesum.go
+++ 3sum/threesum.go
@@ -4,21 +4,21 @@ import "sort"
 
 func threeSum(nums []int) [][]int {
 	var result [][]int
-	var prevTriplet, triplet []int
+	var triplet []int
+	prevBalances := make(map[int]int)
 	sort.Ints(nums)
 	for firstIndex := 0; firstIndex != len(nums); firstIndex += 1 {
 		for secondIndex := firstIndex + 1; secondIndex != len(nums); secondIndex += 1 {
 			balance := nums[firstIndex] + nums[secondIndex]
-			for thirdIndex := secondIndex + 1; thirdIndex != len(nums); thirdIndex += 1 {
+			triplet = nil
+			for thirdIndex := secondIndex + 1; nil == triplet && thirdIndex != len(nums); thirdIndex += 1 {
 				if 0 == nums[thirdIndex]+balance {
 					triplet = []int{nums[firstIndex], nums[secondIndex], nums[thirdIndex]}
-					break
 				}
 			}
-			if 3 == len(triplet) && (nil == prevTriplet || [3]int(prevTriplet) != [3]int(triplet)) {
+			if firstNumber, alreadyFound := prevBalances[balance]; nil != triplet && (alreadyFound != true || firstNumber != triplet[0]) {
+				prevBalances[balance] = triplet[0]
 				result = append(result, triplet)
-				prevTriplet = triplet
-				triplet = nil
 			}
 		}
 	}
