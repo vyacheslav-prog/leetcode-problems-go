637bea89627b229f3e233776396628f087ac7e0d
vyacheslav <vyacheslav.soynov@gmail.com>
2024-06-24 07:55:52 +0700

    feat: find a closest sum for decrease a nums

--- 3sum-closest/threesumclosest.go
+++ 3sum-closest/threesumclosest.go
@@ -1,9 +1,29 @@
 package threesumclosest
 
+import "sort"
+
 func threeSumClosest(nums []int, target int) int {
-	var result int
-	for _, value := range nums {
-		result += value
+	var distance, result, resultDistance, sum int
+	sort.Ints(nums)
+	for firstIndex := 0; firstIndex < len(nums)-2; firstIndex += 1 {
+		for secondIndex, thirdIndex := firstIndex+1, len(nums)-1; secondIndex != thirdIndex; {
+			sum = nums[firstIndex] + nums[secondIndex] + nums[thirdIndex]
+			if sum < target {
+				secondIndex += 1
+			} else if target < sum {
+				thirdIndex -= 1
+			} else {
+				secondIndex = thirdIndex
+			}
+		}
+		distance = target - sum
+		if distance < 0 {
+			distance *= -1
+		}
+		if 0 == firstIndex || distance < resultDistance {
+			result = sum
+			resultDistance = distance
+		}
 	}
 	return result
 }
--- 3sum-closest/threesumclosest_test.go
+++ 3sum-closest/threesumclosest_test.go
@@ -18,9 +18,41 @@ func TestFindsSumForThreeNums(t *testing.T) {
 }
 
 func TestFindsSumThreeForFourNums(t *testing.T) {
-	nums := []int{0, 1, 1, 1}
-	result := threeSumClosest(nums, 0)
+	nums, target := []int{0, 1, 1, 1}, 0
+	result := threeSumClosest(nums, target)
 	if expected := 2; expected != result {
-		t.Errorf("Result must be [%v] for four nums [%v], actual is [%v]", expected, nums, result)
+		t.Errorf("Result must be [%v] for four nums [%v] and target [%v], actual is [%v]", expected, nums, target, result)
+	}
+}
+
+func TestFindsMaxSumThreeForFourNums(t *testing.T) {
+	nums, target := []int{0, 1, 1, 1}, 3
+	result := threeSumClosest(nums, target)
+	if expected := 3; expected != result {
+		t.Errorf("Result must be [%v] for nums [%v] and target [%v], actual is [%v]", expected, nums, target, result)
+	}
+}
+
+func TestFindsClosestSumOfThreeWhenNumsIsUnordered(t *testing.T) {
+	nums, target := []int{1, 1, 1, 0}, 3
+	result := threeSumClosest(nums, target)
+	if expected := 3; expected != result {
+		t.Errorf("Result must be [%v] for unordered nums [%v] and target [%v], actual is [%v]", expected, nums, target, result)
+	}
+}
+
+func TestFindsClosestRightOverflowedSumOfThree(t *testing.T) {
+	nums, target := []int{-1, 2, 1, -4}, 1
+	result := threeSumClosest(nums, target)
+	if expected := 2; expected != result {
+		t.Errorf("Result must be [%v] for overflowed sum of nums [%v] and target [%v], actual is [%v]", expected, nums, target, result)
+	}
+}
+
+func TestFindsClosestSumForFirstAndMinimalTriplet(t *testing.T) {
+	nums, target := []int{2, 3, 8, 9, 10}, 16
+	result := threeSumClosest(nums, target)
+	if expected := 15; expected != result {
+		t.Errorf("Result must be minimal [%v] for nums [%v] and target [%v], actual is [%v]", expected, nums, target, result)
 	}
 }
