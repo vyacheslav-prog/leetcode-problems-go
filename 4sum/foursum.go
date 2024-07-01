6e9b8b2bab028a01b314b0bbb1aa711e91a51b7f
vyacheslav <vyacheslav.soynov@gmail.com>
2024-07-01 08:38:48 +0700

    feat: find a quadruplet for all nums without target

--- 4sum/foursum.go
+++ 4sum/foursum.go
@@ -1,5 +1,8 @@
 package foursum
 
 func fourSum(nums []int, target int) [][]int {
+	if 0 != len(nums) {
+		return [][]int{nums}
+	}
 	return nil
 }
--- 4sum/foursum_test.go
+++ 4sum/foursum_test.go
@@ -8,3 +8,19 @@ func TestFindsZeroQuadrupletsForZero(t *testing.T) {
 		t.Errorf("Result must be empty for empty nums, actual is [%v]", result)
 	}
 }
+
+func TestFindsSingleQuadrupletWhenSumIsEqualToTarget(t *testing.T) {
+	nums, target := []int{1, 1, 1, 1}, 4
+	result := fourSum(nums, target)
+	if expected := [4]int(nums); 1 != len(result) || expected != [4]int(result[0]) {
+		t.Errorf("Result must be [%v] for single quadruplet into nums [%v] and target [%v], actual is [%v]", expected, nums, target, result)
+	}
+}
+
+func TestFindsZeroQuadrupletsWhenTargetIsNotEqualANumsSum(t *testing.T) {
+	nums, target := []int{1, 1, 1, 1}, 5
+	result := fourSum(nums, target)
+	if 0 != len(result) {
+		t.Errorf("Result must be empty for nums [%v] and target [%v], actual is [%v]", nums, target, result)
+	}
+}
