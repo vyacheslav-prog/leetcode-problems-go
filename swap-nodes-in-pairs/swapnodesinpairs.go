85a8e21e56907fb0ee8b5bf5ab5059a2b449749c
vyacheslav <vyacheslav.soynov@gmail.com>
2024-08-08 08:02:33 +0700

    feat: complete swap-nodes-in-pairs problem

--- swap-nodes-in-pairs/swapnodesinpairs.go
+++ swap-nodes-in-pairs/swapnodesinpairs.go
@@ -6,5 +6,24 @@ type ListNode struct {
 }
 
 func swapPairs(head *ListNode) *ListNode {
-	return nil
+	var leftHead, rightHead *ListNode
+	mergedPreHead := &ListNode{0, nil}
+	mergedTail := mergedPreHead
+	for cursor := head; nil != cursor; cursor = cursor.Next {
+		if nil == leftHead {
+			leftHead = cursor
+		} else if nil == rightHead {
+			rightHead = cursor
+		}
+		if nil != rightHead {
+			mergedTail.Next = &ListNode{rightHead.Val, &ListNode{leftHead.Val, nil}}
+			mergedTail = mergedTail.Next.Next
+			leftHead = nil
+			rightHead = nil
+		}
+	}
+	if nil != leftHead {
+		mergedTail.Next = &ListNode{leftHead.Val, nil}
+	}
+	return mergedPreHead.Next
 }
--- swap-nodes-in-pairs/swapnodesinpairs_test.go
+++ swap-nodes-in-pairs/swapnodesinpairs_test.go
@@ -2,6 +2,29 @@ package swapnodesinpairs
 
 import "testing"
 
+func makeListNodeFromNums(nums []int) *ListNode {
+	var cursor, head *ListNode
+	for index, value := range nums {
+		if 0 == index {
+			head = &ListNode{value, nil}
+			cursor = head
+		} else {
+			cursor.Next = &ListNode{value, nil}
+			cursor = cursor.Next
+		}
+	}
+	return head
+}
+
+func makeNumsFromListNode(head *ListNode) []int {
+	var result []int
+	for nil != head {
+		result = append(result, head.Val)
+		head = head.Next
+	}
+	return result
+}
+
 func TestSwapsNotForNilHead(t *testing.T) {
 	result := swapPairs(nil)
 	if nil != result {
@@ -14,5 +37,29 @@ func TestSwapsOneNodeList(t *testing.T) {
 	result := swapPairs(head)
 	if nil == result || 0 != result.Val {
 		t.Errorf("Result must be same head [%v] after swap, actual is [%v]", head, result)
+	}
+}
+
+func TestSwapsTwoNodeList(t *testing.T) {
+	nums := []int{0, 1}
+	result := swapPairs(makeListNodeFromNums(nums))
+	if expectedNums, resultNums := [2]int{1, 0}, makeNumsFromListNode(result); 2 != len(resultNums) || expectedNums != [2]int(resultNums) {
+		t.Errorf("Result must have nums [%v] for list with nums [%v], actual is [%v]", expectedNums, nums, resultNums)
+	}
+}
+
+func TestSwapsTwoHeadNodesIntoThreeNodeList(t *testing.T) {
+	nums := []int{-2, -1, -3}
+	result := swapPairs(makeListNodeFromNums(nums))
+	if expectedNums, resultNums := [3]int{-1, -2, -3}, makeNumsFromListNode(result); 3 != len(resultNums) || expectedNums != [3]int(resultNums) {
+		t.Errorf("Result must have nums [%v] for list with nums [%v], actual is [%v]", expectedNums, nums, resultNums)
+	}
+}
+
+func TestSwapsTwoPairNodesIntoFourNodeList(t *testing.T) {
+	nums := []int{1, 0, 3, 2}
+	result := swapPairs(makeListNodeFromNums(nums))
+	if expectedNums, resultNums := [4]int{0, 1, 2, 3}, makeNumsFromListNode(result); 4 != len(resultNums) || expectedNums != [4]int(resultNums) {
+		t.Errorf("Result must have nums [%v] for list with nums [%v], actual is [%v]", expectedNums, nums, resultNums)
 	}
 }
