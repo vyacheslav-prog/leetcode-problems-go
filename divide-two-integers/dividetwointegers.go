3552993b52d498e69d3e0fd3c4acae902bc9414b
vyacheslav <vyacheslav.soynov@gmail.com>
2024-09-16 07:50:47 +0700

    feat: divides integers with positive overflow

--- divide-two-integers/dividetwointegers.go
+++ divide-two-integers/dividetwointegers.go
@@ -1,9 +1,26 @@
 package dividetwointegers
 
+const positiveMax = 1 << 31 - 1
+
 func divide(dividend int, divisor int) int {
+	var isNegativeDividend, isNegativeDivisor bool
 	var result int
+	if dividend < 0 {
+		dividend = -dividend
+		isNegativeDividend = true
+	}
+	if divisor < 0 {
+		divisor = -divisor
+		isNegativeDivisor = true
+	}
 	for remainder := dividend; divisor <= remainder; remainder -= divisor {
 		result += 1
+	}
+	if isNegativeDividend != isNegativeDivisor {
+		result = -result
+	}
+	if 0 < result && 0 != result >> 31 {
+		result = positiveMax
 	}
 	return result
 }
--- divide-two-integers/dividetwointegers_test.go
+++ divide-two-integers/dividetwointegers_test.go
@@ -41,3 +41,35 @@ func TestCalcsToNegativeResultForNegativeDivisor(t *testing.T) {
 		t.Errorf("Result must be [%v] for dividend [%v] and negative divisor [%v], actual is [%v]", expected, dividend, divisor, result)
 	}
 }
+
+func TestCalcsToNegativeResultForNegativeDividend(t *testing.T) {
+	dividend, divisor := -3, 1
+	result := divide(dividend, divisor)
+	if expected := -3; expected != result {
+		t.Errorf("Result must be [%v] for negative dividend [%v] and divisor [%v], actual is [%v]", expected, dividend, divisor, result)
+	}
+}
+
+func TestCalcsToPositiveResultForNegativeDividendAndDivisor(t *testing.T) {
+	dividend, divisor := -4, -2
+	result := divide(dividend, divisor)
+	if expected := 2; expected != result {
+		t.Errorf("Result must be [%v] for dividend [%v] and divisor [%v], actual is [%v]", expected, dividend, divisor, result)
+	}
+}
+
+func TestHitsMaximumForPositiveOverflow(t *testing.T) {
+	dividend, divisor := -2147483648, -1
+	result := divide(dividend, divisor)
+	if expected := 2147483647; expected != result {
+		t.Errorf("Result must be [%v] for dividend [%v] and divisor [%v], actual is [%v]", expected, dividend, divisor, result)
+	}
+}
+
+func TestHitsMinimumForNegativeOverflow(t *testing.T) {
+	dividend, divisor := 2147483649, -1
+	result := divide(dividend, divisor)
+	if expected := -2147483648; expected != result {
+		t.Errorf("Result must be [%v] for dividend [%v] and divisor [%v], actual is [%v]", expected, dividend, divisor, result)
+	}
+}
