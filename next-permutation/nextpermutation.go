8805e167b2d213db1ecd40781acdde82653939e3
vyacheslav <vyacheslav.soynov@gmail.com>
2024-10-04 07:51:59 +0700

    feat: find a next permutation for two nums

--- next-permutation/nextpermutation.go
+++ next-permutation/nextpermutation.go
@@ -1,4 +1,7 @@
 package nextpermutation
 
 func nextPermutation(nums []int) {
+	if 2 == len(nums) {
+		nums[0], nums[1] = nums[1], nums[0]
+	}
 }
--- next-permutation/nextpermutation_test.go
+++ next-permutation/nextpermutation_test.go
@@ -9,3 +9,21 @@ func TestReplacesZeroNumsForNilNums(t *testing.T) {
 		t.Errorf("Result must be nil for empty nums, actual is [%v]", nums)
 	}
 }
+
+func TestPermutatesForTwoNums(t *testing.T) {
+	nums := []int{1, 2}
+	result := nums
+	nextPermutation(nums)
+	if expected := [2]int{2, 1}; 2 != len(result) || expected != [2]int(result) {
+		t.Errorf("Result must be [%v] for two nums [%v], actual is [%v]", expected, nums, result)
+	}
+}
+
+func TestPermutatesForRightNumsForThreeNums(t *testing.T) {
+	nums := []int{5, 10, 15}
+	result := nums
+	nextPermutation(nums)
+	if expected := [3]int{5, 15, 10}; 3 != len(result) || expected != [3]int(result) {
+		t.Errorf("Result must be [%v] for nums [%v], actual is [%v]", expected, nums, result)
+	}
+}
